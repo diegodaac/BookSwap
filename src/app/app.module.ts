4e6899cefa59db4b48c84f37acf0e5b50f8d5169
Diego Alvarado <diegoalejandroac28@gmail.com>
2024-02-25 00:54:30 -0600

    Estado Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { AngularFireModule } from '@angular/fire/compat';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-/* import { AngularFireAuthModule } from '@angular/fire/auth'; */
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule
-  , AngularFireModule, 
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
